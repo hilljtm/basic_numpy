80aae39b2560bd702047de9cbb9ca8f643d46cc4
hilljtm <47020214+hilljtm@users.noreply.github.com>
2019-06-23 14:51:07 -0400

    more basics

--- arrays.py
+++ arrays.py
@@ -22,7 +22,64 @@ print('itemsize: ', x3.itemsize, 'bytes') # itemsize = 8 bytes
 print('nbytes: ', x3.nbytes, 'bytes')  # nbytes = 480 bytes
 # in general, we expect nbytes is equal to itemsize times size
 
+# Array Indexing: accessing single elements
+x1  # = array([5, 0, 3, 3, 7, 9])
+x1[0]  # = 5
+x1[4]  # = 7
 
+# indexing from the end of the array
+x1[-1]  # = 9
+x1[-2] # = 7
+
+# multi dimensional array by using comma seperated tuple of indices 
+x2  # = array([[3, 5, 2, 4]])
+            #  [7, 6, 8, 8]
+            #  [1, 6, 7, 7]])
+
+x2[0, 0]  # = 3
+x2[2, 0]  # = 1
+x2[2, -1]  # = 7
+
+# modifying values using any of above index notation
+x2[0, 0] = 12
+x2  # = array([[12, 5, 2, 4],
+            #  [7, 6, 8, 8],
+            #  [1, 6, 7, 7]])
+
+#NOTE: unlike python lists, numpy arrays have a fixed type
+# attempt to insert a floating-point value to an integer array, the value will be silently truncated
+x1[0] = 3.14159  # this will be truncated
+x1  # = array([5, 0, 3, 3, 7, 9])
+
+# Array slicing: accessing subarrays 
+#NOTE: slicing syntax,if any are unspecified, the default values are start=0, stop=size of dimension, step=1
+# x[start:stop:step]
+
+# One dimensional subarrays
+x = np.arange(10)
+x  # = array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+# first five elements
+x[:5]  # = array([0, 1, 2, 3, 4])
+
+# elements after index 5
+x[5:]  # = array([5, 6, 7, 8, 9])
+
+# middle subarray
+x[4:7]  # = array([4, 5, 6])
+
+# every other element
+x[::2]  # = array([0, 2, 4, 6, 8])
+
+# every other element, starting at index 1
+x[1::2]  # = array([1, 3, 5, 7, 9])
+
+#NOTE: negative step value defaults for start and stop to be swapped, allowing a way to reverse an array
+# all elements reversed
+x[::-1]  # = array([9, 8, 7, 6, 5, 4, 3, 2, 1, 0])
+
+# reversed every other from index 5
+x[5::-2]  # = array([5, 3, 1])
 
 
 
